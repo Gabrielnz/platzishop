b3d608d941c28f5600a44568d2d465708de06ef7
Gabriel Nunez <gabriel.nzn@gmail.com>
2017-06-05 11:59:17 -0400

    Agregando una seccion de filtros en Catalogo

--- js/app.js
+++ js/app.js
@@ -31,9 +31,24 @@ function mostrarModal() {
 
   if(!noMostrarModal) {
     $('#modalOferta').modal({ backdrop: 'static', keyboard: false })
-  
+
     $('#btnNoRegistrar').click(function (ev) {
       localStorage.noMostrarModal = true
     })
   }
 }
+
+const $filtrosToggle = $('#filtrosToggle')
+
+$filtrosToggle.click(function (ev) {
+  ev.preventDefault()
+
+  const $i = $filtrosToggle.find('i.fa')
+  const isDown = $i.hasClass('fa-chevron-down')
+
+  if (isDown) {
+    $i.removeClass('fa-chevron-down').addClass('fa-chevron-up')
+  } else {
+    $i.removeClass('fa-chevron-up').addClass('fa-chevron-down')
+  }
+})
